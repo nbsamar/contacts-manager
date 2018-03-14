b72c980de1bf82eacdb13e0376d4c07fa30bd369
Nayab <nbsamar@gmail.com>
2018-03-14 16:36:50 +0530

    custom devise fields

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,23 @@
 class ContactsController < ApplicationController
+  def index
+
+  end
+  def new
+
+  end
+  def create
+
+  end
+  def edit
+
+  end
+  def update
+
+  end
+  def show
+
+  end
+  def destroy
+
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,22 +2,9 @@ class UsersController < ApplicationController
   def index
 
   end
-  def new
 
-  end
-  def create
-
-  end
-  def edit
-
-  end
-  def update
-
-  end
   def show
 
   end
-  def destroy
 
-  end
 end
