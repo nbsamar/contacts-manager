5d7d5fa618b6ccc205245d4edcff9bf524e654d6
Nayab <nbsamar@gmail.com>
2018-03-15 18:35:10 +0530

    contacts migration bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  # devise_scope :user do
+  #   root 'devise/sessions#new'
+  # end
   devise_for :users, :controllers => {registrations: 'registrations'}
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#new'
+  root 'users#show'
 end
--- db/migrate/20180310191026_create_contacts.rb
+++ db/migrate/20180310191026_create_contacts.rb
@@ -3,7 +3,7 @@ class CreateContacts < ActiveRecord::Migration[5.1]
     create_table :contacts do |t|
       t.string :name
       t.string :email
-
+      t.belongs_to :user, index: true
       t.timestamps
     end
   end
