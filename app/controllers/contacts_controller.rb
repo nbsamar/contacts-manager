6b3d95ff5c453bb41d3368e6c621649bf12c7127
Nayab <nbsamar@gmail.com>
2018-03-22 13:29:29 +0530

    nested attributes for addresses and phone numbers

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,10 +6,16 @@ class ContactsController < ApplicationController
   end
   def new
     @contact = Contact.new
+    @phone_number = PhoneNumber.new(phone_number_params)
   end
   def create
+    # @address = Address.new(address_params)
     @contact = Contact.new(contact_params)
     @contact.user = current_user
+    @phone_number = PhoneNumber.new(phone_number_params)
+    @phone_number.contact = @contact.id
+    # @address.contact = contact
+    # @contact.phone_numbers.build
     if @contact.save
       flash[:notice] = 'Contact Added'
       redirect_to new_contact_path
@@ -30,8 +36,14 @@ class ContactsController < ApplicationController
   def destroy
 
   end
-  private
+  # private
   def contact_params
     params.require(:contact).permit(:givenName, :familyName, :email)
   end
+  def phone_number_params
+    params.require(:phone_number).permit(:phoneNumber)
+  end
+  # def address_params
+  #   params.require(:address).permit(:city, :street, :region, :postcode, :country)
+  # end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,6 @@ class Contact < ApplicationRecord
   belongs_to :user
   has_many :addresses
   has_many :phone_numbers
+
+  accepts_nested_attributes_for :addresses, :phone_numbers
 end
